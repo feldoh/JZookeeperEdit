6459769e8c3e321330c4fcedbac7f4f7d5f676c7
Dexter Lowe <dexter.lowe@imagini.net>
2015-09-08 18:54:21 +0100

    Cleaned imports

--- src/main/java/net/imagini/jzookeeperedit/MainApp.java
+++ src/main/java/net/imagini/jzookeeperedit/MainApp.java
@@ -1,7 +1,6 @@
 package net.imagini.jzookeeperedit;
 
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
