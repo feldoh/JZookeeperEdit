4cf630a20dcf9378e17b967b91b0ac5bad56938f
Dexter Lowe <dexter.lowe@imagini.net>
2015-09-08 17:12:25 +0100

    Simplified window management

--- src/main/java/net/imagini/jzookeeperedit/FXSceneManager.java
+++ src/main/java/net/imagini/jzookeeperedit/FXSceneManager.java
@@ -1,25 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.imagini.jzookeeperedit;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
-import javafx.beans.property.DoubleProperty;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
-import javafx.util.Duration;
 
 /**
  *
@@ -67,41 +55,14 @@ public class FXSceneManager extends StackPane {
     public boolean setScene(final SCENE scene) {
         // If there is already a screen loaded
         if (scenes.get(scene) != null) {
-            final DoubleProperty opacity = opacityProperty();
-
-            //Is there is more than one screen 
+            //If there is an existing scene remove it first
             if (!getChildren().isEmpty()) {
-                Timeline fade = new Timeline(
-                        new KeyFrame(Duration.ZERO,
-                                new KeyValue(opacity, 1.0D)),
-                        new KeyFrame(new Duration(1000),
-                                new EventHandler() {
-                                    @Override
-                                    public void handle(Event t) {
-                                        //remove displayed screen 
-                                        getChildren().remove(0);
-                                        //add new screen 
-                                        getChildren().add(0, scenes.get(scene));
-                                        Timeline fadeIn = new Timeline(
-                                                new KeyFrame(Duration.ZERO,
-                                                        new KeyValue(opacity, 0.0D)),
-                                                new KeyFrame(new Duration(800),
-                                                        new KeyValue(opacity, 1.0D)));
-                                        fadeIn.play();
-                                    }
-                                }, new KeyValue(opacity, 0.0D)));
-                fade.play();
-            } else {
-                //no one else been displayed, then just show 
-                setOpacity(0.0D);
-                getChildren().add(scenes.get(scene));
-                Timeline fadeIn = new Timeline(
-                        new KeyFrame(Duration.ZERO,
-                                new KeyValue(opacity, 0.0D)),
-                        new KeyFrame(new Duration(2500),
-                                new KeyValue(opacity, 1.0D)));
-                fadeIn.play();
+                //remove displayed screen
+                getChildren().remove(0);
             }
+
+            //add new screen
+            getChildren().add(0, scenes.get(scene));
             return true;
         } else {
             System.out.println("screen hasn't been loaded!\n");
